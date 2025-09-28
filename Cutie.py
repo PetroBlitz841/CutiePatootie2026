210f65b3cccbdb12dce4b1c94be21c0ce0fd87fa
PetroBlitz841 <78737726+PetroBlitz841@users.noreply.github.com>
2025-09-28 13:16:20 +0300

    Update Cutie.py

--- Cutie.py
+++ Cutie.py
@@ -1,1 +1,23 @@
+'''
+CUTIE PATOOTIE 2026
+'''
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
 
+hub = PrimeHub()
+
+#CUTIE WHEELS
+left_wheel = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_wheel = Motor(Port.E, Direction.CLOCKWISE)
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.F)
+
+
+
+cutie = DriveBase(left_wheel,right_wheel, 62.4, 80)
+
+cutie.straight(400)
