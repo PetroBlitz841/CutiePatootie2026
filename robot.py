88916e041d1534bcb4dc21a3f7173f3642405a0e
PetroBlitz841 <78737726+PetroBlitz841@users.noreply.github.com>
2025-12-02 15:16:54 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -14,21 +14,29 @@ left_motor = Motor(Port.B, gears=[20,28])
 right_motor = Motor(Port.F, gears=[20,28])
 
 sensor = ColorSensor(Port.C)
+sensor2 = ColorSensor(Port.D)
+
+# while sensor2.reflection() > 20:
+#         print(sensor2.reflection())
+
 
 
 cutie = DriveBase(left_wheel,right_wheel, 62.4, 80)
-Color.RED = Color(343, 82, 36)
-Color.BLUE = Color(216,88,27)
-Color.GREEN = Color(156, 72, 19)
+color_list= [
+    Color(343, 82, 36), #red
+    Color(216,88,27),  #blue
+    Color(156, 72, 19), #green
+    Color(51, 75, 71), #yellow
+]
 run_colors = (
     Color.BLUE,
     Color.GREEN,
-    Color.RED,
+    Color.RED
 )
-
+sensor2.detectable_colors(color_list)
 
 # while("1 + 1 = 3"):
-#     print(sensor.hsv())
+#     print(sensor2.hsv())
 # functions
 # def GetOut():
 #     cutie.drive(-50, 0)
@@ -55,18 +63,91 @@ def straight_time(speed, time):
 
     cutie.settings(last)
 
+def till_black(speed, turn_rate):
+    cutie.drive(speed, turn_rate)
 
+    while sensor2.reflection() > 7:
+        print(sensor2.reflection())
+        pass
+
+    cutie.stop()
+
+def abs_turn(target):
+    distance = target - cutie.angle()
+    
+    cutie.turn(turnDeg)
+
+def turn_to(angle):
+    print(hub.imu.heading())
+    start_angle = (hub.imu.heading() + 360) % 360  # 208
+    print(start_angle)
+    deg_to_turn = (angle - start_angle) % 360  # 242
+    print(deg_to_turn)
+
+    if deg_to_turn >= 180:
+        chassis.turn(deg_to_turn - 360)
+    else:
+        chassis.turn(deg_to_turn)
+
+def turn_to_angle(angle, speed=200, max_time=3):
+    """Turns to a specified absolute gyro angle"""
+
+    timer = StopWatch()
+    timer.reset()
+
+    distance = angle - hub.imu.heading()
+    robot_acceleration = wheels.settings()[3]
+    wheels.settings(turn_rate=speed)
+    wheels.turn(distance)
+
+    # while (timer.time()) < (max_time * 1000) and angle - hub.imu.heading() > 1:
+    #     ...
+    wheels.settings(turn_rate=robot_acceleration)
+
+def is_color_in_range(measured_color:Color, comparison_color:Color, range:int):
+    h = comparison_color.h - range <= measured_color.h <= comparison_color.h + range
+    s = comparison_color.s - range <= measured_color.s <= comparison_color.s + range
+    v = comparison_color.v - range <= measured_color.v <= comparison_color.v + range
+
+    return (h & s & v)
+
+def GetOut():
+    cutie.drive(-50, 0)
+
+    #while not is_color_in_range(sensor2.color, yellow, 10):
+    while sensor2.color!= Color(52, 75, 77):
+        # print(sensor2.hsv())
+        print(sensor2.hsv())
+        pass
+
+    left_wheel.hold()
+    right_motor.hold()
+
+# GetOut()
+# till_black(100, 0)
 def run1():
-    straight_time(1000, 6000)
-    wait(2000)
-    left_motor.run_angle(-500, 90)
+    cutie.settings(700)
+    cutie.straight(1300)
+    straight_time(300, 4000)
+    wait(1000)
+    left_motor.run_angle(-500, 60)
+    wait(1000)
+    left_motor.run_angle(500, 120)
+    wait(4000)
+    left_motor.run_angle(-500, 60)
     cutie.settings(150)
     cutie.straight(-2000)
 
 def run2():
-    print("gyatt")
+    cutie.use_gyro(True)
+    cutie.settings(80)
+    cutie.straight(-600)
 def run3():
-    print("gyatt")
+    cutie.straight(700)
+    till_black(100, 0)
+    cutie.turn(80)
+    cutie.straight(300)
+
 # left_motor.run_time(-5000, 3000)
 
 # cutie.settings(50)
