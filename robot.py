b8981e5f4e3c922f8cf9312c258a7ab472fa6a42
PetroBlitz841 <78737726+PetroBlitz841@users.noreply.github.com>
2026-02-03 20:35:32 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -20,6 +20,18 @@ sensor2 = ColorSensor(Port.B)
 
 
 cutie = DriveBase(left_wheel, right_wheel, 62.4, 80)
+# def gyro_abs(target, speed, kp):
+#     while not (target > hub.imu.heading() - 0.03) or not (target < hub.imu.heading() + 0.03): # the stoping range
+#         print(hub.imu.heading())
+#         direction = (target - (hub.imu.heading() % 360)) % 360 # calculating the direction of the turn
+#         if direction > 180:
+#             error = abs(hub.imu.heading() - target) * kp
+#             left_wheel.dc(-speed * error / 10)
+#             right_wheel.dc(speed * error/ 10)
+#         else:
+#             left_wheel.dc(speed)
+#             right_wheel.dc(-speed)
+#     cutie.stop()
 
 
 color_list = [
@@ -32,7 +44,20 @@ run_colors = (Color.BLUE, Color.GREEN, Color.RED)
 sensor2.detectable_colors(color_list)
 
 cutie.use_gyro(True)
+# while("1 + 1 = 3"):
+#     print(sensor2.hsv())
+# functions
+# def GetOut():
+#     cutie.drive(-50, 0)
 
+#     while sensor.color() != Color.MAGENTA:
+#         print(sensor.color())
+#         print(sensor.hsv())
+#         pass
+
+#     cutie.stop()
+
+# right_motor.run_time(5000, 5000, wait=False)
 def straight_time(speed, time):
     timer = StopWatch()
     last = cutie.settings()[0]
@@ -66,6 +91,62 @@ def till_black(speed, turn_rate):
 
     cutie.stop()
 
+
+
+def till_red(speed, turn_rate):
+    cutie.drive(speed, turn_rate)
+
+    while hub.imu.tilt()[1] < -4 or hub.imu.tilt()[1] > 4:
+        print(hub.imu.tilt())
+        pass
+
+    cutie.stop()
+# till_red(-50, 0)
+
+
+# def abs_turn(target):
+#     distance = target - cutie.angle()
+
+#     cutie.turn(turnDeg)
+
+# def turn_to(angle):
+#     print(hub.imu.heading())
+#     start_angle = (hub.imu.heading() + 360) % 360  # 208
+#     print(start_angle)
+#     deg_to_turn = (angle - start_angle) % 360  # 242
+#     print(deg_to_turn)
+
+#     if deg_to_turn >= 180:
+#         chassis.turn(deg_to_turn - 360)
+#     else:
+#         chassis.turn(deg_to_turn)
+
+# def turn_to_angle(angle, speed=200, max_time=3):
+#     """Turns to a specified absolute gyro angle"""
+
+#     timer = StopWatch()
+#     timer.reset()
+
+#     distance = angle - hub.imu.heading()
+#     robot_acceleration = wheels.settings()[3]
+#     wheels.settings(turn_rate=speed)
+#     wheels.turn(distance)
+
+#     # while (timer.time()) < (max_time * 1000) and angle - hub.imu.heading() > 1:
+#     #     ...
+#     wheels.settings(turn_rate=robot_acceleration)
+# gyro_abs(90, 15)
+# print(hub.imu.heading())
+# wait(1000)
+# print(hub.imu.heading())
+
+def is_color_in_range(measured_color: Color, comparison_color: Color, range: int): # pylint: disable=redefined-builtin
+    h = comparison_color.h - range <= measured_color.h <= comparison_color.h + range
+    s = comparison_color.s - range <= measured_color.s <= comparison_color.s + range
+    v = comparison_color.v - range <= measured_color.v <= comparison_color.v + range
+
+    return h & s & v
+
 def turn_to(angle, then=Stop.HOLD):
     print(hub.imu.heading())
     start_angle = (hub.imu.heading() + 360) % 360  # 208
@@ -82,7 +163,35 @@ def turn_to(angle, then=Stop.HOLD):
         cutie.turn(deg_to_turn - 360)
     else:
         cutie.turn(deg_to_turn)
-        
+# cutie.settings(turn_rate=40)
+# cutie.turn(360)
+# # turn_to(90)
+# # hub.imu.reset_heading(0)
+# # wait(50)
+# gyro_abs(57, 40)
+# gyro_abs(0, 40)
+
+def GetOut():
+    cutie.drive(-50, 0)
+
+    # while not is_color_in_range(sensor2.color, yellow, 10):
+    while sensor2.color() != Color(52, 75, 77):
+        # print(sensor2.hsv())
+        print(sensor2.hsv())
+
+    left_wheel.hold()
+    right_motor.hold()
+
+# left_motor.run_time(1000, 5000, wait = False)
+# cutie.straight(50)
+# left_motor.run_time(100, 5000, wait=False)
+# cutie.turn(120)
+# cutie.curve(radius=600, angle=-30)
+# cutie.straight(150)
+# # right_motor.run_time(300, 3000)
+# left_motor.run_time(-5000, 5000)
+# left_motor.run_time(5000, 5000)
+
 def gyro_abs(target_angle, speed=100, kp=1.5, ke = 20):
         
     while True:
@@ -103,11 +212,30 @@ def gyro_abs(target_angle, speed=100, kp=1.5, ke = 20):
     cutie.stop()
     wait(1000)
     print(hub.imu.heading())
+    
+
+# gyro_abs(90)
+
+# Example: Turn 90 degrees right
+# cutie.straight(-3000)
+# wait(5000)
+# cutie.straight(200)
+# cutie.straight(-2000)
+# # GetOut()
+# till_black(100, 0)
+# right_motor.run_time(300, 5000)
+# left_motor.run_time(-300, 5000)
+    # right_motor.run_time(300, 5000)
+# cutie.curve(-6000, -60)
+# gyro_abs(90)
+# cutie.settings(500)
+# cutie.straight(5000)
+# cutie.curve(20, 1000)
 
 def run1():
     cutie.settings(1000)
-    cutie.straight(1300, then = Stop.NONE)
-    straight_time(600, 4000) #go with merkava
+    cutie.straight(distance=1300)
+    straight_time(600, 4000)
     wait(100)
     cutie.settings(150, turn_rate= 40)
     cutie.use_gyro(True)
@@ -123,7 +251,7 @@ def run1():
     gyro_abs(0, 150, ke=25)
     cutie.straight(-650)
     cutie.settings(turn_acceleration = 200)
-    right_motor.run_time(300, 5000, wait=False)
+    right_motor.run_time(speed=300, time=5000, wait=False)
     cutie.use_gyro(False)
     cutie.settings(turn_rate=50)
     turn_to(90)
@@ -137,8 +265,8 @@ def run1():
     cutie.use_gyro(True)
     till_black(65, 0)
     cutie.straight(10)
-    left_motor.run_time(-2000, 3500, wait=False) # drop big arm for M02
-    right_motor.run_angle(-160, 400) #lift valuable items
+    left_motor.run_time(-2000, 3500, wait=False)
+    right_motor.run_angle(-160, 400)
     right_motor.run_time(200, 2000)
     right_motor.run_angle(-160, 140)
     cutie.settings(turn_rate=70, straight_speed= 80)
@@ -163,21 +291,22 @@ def run1():
 def run2():
     # cutie.straight(-300)
     # cutie.turn(-20)
-    curve_time(3000, 15) # curve into wall, to actually folloow
-    right_motor.run_time(-1000, 1000) # drop flag in boat
+    curve_time(3000, 15)
+    right_motor.run_time(-1000, 1000)
     cutie.straight(-500, then=Stop.NONE)
     cutie.use_gyro(True)
-    cutie.curve(500, -45)
-    cutie.curve(500, 45)
+    cutie.curve(500, -25)
+    cutie.straight(550)
     gyro_abs(0, 250, ke=25)
-    till_black(100, 0) # go until black line
+    cutie.straight(50)
+    till_black(100, 0)
     cutie.settings(straight_speed=100, turn_rate=80)
     cutie.straight(50)
-    gyro_abs(-90, 250, ke=25) # turn to to fish items
+    gyro_abs(-90, 250, ke=25)
     till_black(-100, 0)
     cutie.straight(-25)
     cutie.turn(-30)
-    right_motor.run_time(-1000, 3000) # fish items
+    right_motor.run_time(-1000, 3000)
     cutie.use_gyro(True)
     cutie.settings(80)
     cutie.straight(30)
@@ -186,10 +315,10 @@ def run2():
     cutie.straight(100)
     gyro_abs(0, 250, ke=20)
     till_black(-100,0)
-    cutie.straight(195) # go into plate thing
+    cutie.straight(195)
     cutie.straight(-100)
     cutie.settings(210)
-    cutie.turn(45) # get out the plate thing
+    cutie.turn(45)
     cutie.straight(40,then=Stop.NONE)
     cutie.curve(60, -45, then=Stop.NONE)
     cutie.straight(350)
@@ -199,16 +328,17 @@ def run2():
     straight_time(-100, 1500)
     cutie.straight(30)
     cutie.settings(turn_rate=200)
-    cutie.turn(-60) # lift up items
+    cutie.turn(-60)
     cutie.straight(1000)
-
 
 
 
 def run3():
     cutie.settings(straight_speed=1000)
     cutie.straight(700)
-    till_black(100, 0) #go until black line
+    # right_motor.run_until_stalled(300, duty_limit=50)
+    # wait(2000)
+    till_black(100, 0)
     gyro_abs(48, 30)
     cutie.settings(straight_speed=200)
     cutie.straight(300)
@@ -225,9 +355,13 @@ def victory_dance():
     cutie.drive(0, 360)
     while True:
         wait(100)
+# left_motor.run_time(-5000, 3000)
+
+# cutie.settings(50)
+# cutie.straight(-2000)
 
 
-#run_selection
+# GetOut()
 def cycle(iterable):
     iterator = iter(iterable)
     while True:
